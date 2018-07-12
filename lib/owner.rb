77aa25d381048d0dd137eb03742d127c6fbd2ef1
Ethel C <cofresi.ethel@gmail.com>
2018-07-12 13:27:44 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,25 @@
 class Owner
-  # code goes here
+  
+  # Class Methods #
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def self.count
+    @@all.size
+  end
+end
+
+  
+  
+  
+  
+  
+  
+  
 end
