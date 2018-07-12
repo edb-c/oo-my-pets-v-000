10f0f3c97aee3e09765d98d24782f5a9d2f826eb
Ethel C <cofresi.ethel@gmail.com>
2018-07-12 14:14:52 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -55,7 +55,7 @@ class Owner
   end
   
     def sell_pets
-    self.pets.each do |pet_type, pets|
+    self.pets.each do |species, each_species|
       pets.each do |pet|
         pet.mood = "nervous"
       end
