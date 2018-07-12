85ae0d5bb5abda5bc437c3ebf356f97eb3661b9e
Ethel C <cofresi.ethel@gmail.com>
2018-07-12 13:51:53 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -42,6 +42,18 @@ class Owner
     @pets[:cats] << Cat.new(cat_name)
   end 
 
+    def walk_dogs
+    self.pets[:dogs].each {|dog| dog.mood = "happy"}
+  end
+
+  def play_with_cats
+    self.pets[:cats].each {|cat| cat.mood = "happy"}
+  end
+
+  def feed_fish
+    self.pets[:fishes].each {|fish| fish.mood = "happy"}
+  end
+  
   
 
 
