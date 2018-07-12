fb1b5fe084be45df02d4d015ced9b991a6a35025
Ethel C <cofresi.ethel@gmail.com>
2018-07-12 13:41:50 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -29,6 +29,18 @@ class Owner
    def say_species
     "I am a #{self.species}."
    end
+   
+    def buy_fish(fish_name)
+    @pets[:fishes] << Fish.new(fish_name)
+  end
+
+  def buy_dog(dog_name)
+    @pets[:dogs] << Dog.new(dog_name)
+  end
+
+  def buy_cat(cat_name)
+    @pets[:cats] << Cat.new(cat_name)
+  end 
 
 
 
